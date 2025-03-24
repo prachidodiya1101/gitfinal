903c574ad4e1c876c510cea4435160c8a66a6198
ansh <prachidodiya123@gmail.com>
2025-03-24 13:14:08 +0530

    Added set.sh file

---
+++ set.sh
@@ -0,0 +1,27 @@
+#!/bin/nash
+
+set laptop mobile tablet
+
+echo"latop: $1"
+echo"mobile: $2"
+echo"tablet: $3"
+
+
+var=ajay
+echo Hello,$var
+
+a=10
+b=20
+
+if [ [ $a -gt 8 && $b -lt 15 ] ]
+then
+	echo"condition is true."
+else
+	echo"condition is false."
+fi
+
+
+
+
+===============================
+# this is my practice file
